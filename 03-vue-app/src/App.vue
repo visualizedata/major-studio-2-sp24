971dd4c64f588d2a3802a2f721811648166d9955
Daniel Sauter <daniel@climasens.com>
2024-02-07 22:35:59 -0500

    add bike-decision branch

--- 03-vue-app/src/App.vue
+++ 03-vue-app/src/App.vue
@@ -1,9 +1,52 @@
-<template><div></div></template>
+<template>
+  <div>
+    <h1>Making decisions based on the weather forecast</h1>
+    <h3>Next daytime temperature: {{ firstDaytimePeriod.temperature }}</h3>
+    <BikeRecommender :recommendation="recommendation" />
+  </div>
+</template>
 
 <script>
+import BikeRecommender from "./components/BikeRecommender.vue";
+
+const API_URL = "https://api.weather.gov/gridpoints/OKX/33,37/forecast";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    BikeRecommender,
+  },
+  data() {
+    return {
+      forecast: null,
+    };
+  },
+  computed: {
+    firstDaytimePeriod() {
+      if (!this.forecast) {
+        return {};
+      }
+      console.log(this.forecast);
+      const daytimePeriod = this.forecast.properties.periods.find(
+        (d) => d.isDaytime
+      );
+      return daytimePeriod || {};
+    },
+    recommendation() {
+      const { temperature } = this.firstDaytimePeriod;
+      if (temperature === undefined) {
+        return null;
+      }
+      return temperature >= 45 && temperature < 80;
+    },
+  },
+  mounted() {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        this.forecast = data;
+      });
+  },
 };
 </script>
 
