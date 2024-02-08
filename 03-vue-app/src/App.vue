dae4185e7d33d2eace66dbd17cc1b918552180f0
Daniel Sauter <daniel@climasens.com>
2024-02-07 22:27:50 -0500

    add umbrella-decision branch

--- 03-vue-app/src/App.vue
+++ 03-vue-app/src/App.vue
@@ -1,9 +1,49 @@
-<template><div></div></template>
+<template>
+  <div>
+    <h1>Making decisions based on the weather forecast</h1>
+    <UmbrellaRecommender :recommendation="recommendation" />
+  </div>
+</template>
 
 <script>
+import UmbrellaRecommender from "./components/UmbrellaRecommender.vue";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      forecast: null,
+    };
+  },
+  components: {
+    UmbrellaRecommender,
+  },
+  computed: {
+    umbrellaRecommendation() {
+      if (
+        !this.forecast ||
+        !this.forecast.properties ||
+        !this.forecast.properties.periods ||
+        !this.forecast.properties.periods.length
+      ) {
+        return false;
+      }
+      const nextDaytimeForecast = this.forecast.properties.periods.find(
+        (d) => d.isDaytime
+      );
+      if (!nextDaytimeForecast) {
+        return false;
+      }
+      return nextDaytimeForecast.shortForecast.includes("Rain");
+    },
+  },
+  mounted() {
+    fetch("https://api.weather.gov/gridpoints/OKX/33,37/forecast")
+      .then((res) => res.json())
+      .then((data) => {
+        this.forecast = data;
+      });
+  },
 };
 </script>
 
