567c702ae10c58a3bfb006918df1d41632297426
Daniel Sauter <daniel@climasens.com>
2024-03-17 13:09:23 -0400

    add lab06

---
+++ 06-vue-scrollytelling/vue-app/src/components/Forecast.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-card
+    class="card"
+    shadow="always"
+    :body-style="{
+      
+    }"
+  >
+    <div>
+      <strong>
+        Section {{data.number}}
+      </strong> 
+      ({{ parseInt(progress*100) }}%)
+      <h2>
+        <p>{{data.name}}</p>
+      </h2>
+      <p>{{data.detailedForecast}}</p>
+      <img 
+        :src="data.icon"
+        :alt="data.shortForecast"
+        :style="{opacity: 1-progress}"
+      />
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "Forecast",
+  props: {
+    data: Object,
+    progress: Number
+  }
+};
+</script>
+
+<style></style>
